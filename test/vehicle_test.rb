9805220cff93e4054ee8747f9ab7e2a923c65585
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-14 15:27:18 -0600

    Add add_passengers test to Vehicle

--- test/vehicle_test.rb
+++ test/vehicle_test.rb
@@ -1,8 +1,12 @@
 require './test/test_helper'
 require './lib/vehicle'
+require './lib/passenger'
 class VehicleTest < Minitest::Test
   def setup
     @vehicle = Vehicle.new("2001", "Honda", "Civic")
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @jude = Passenger.new({"name" => "Jude", "age" => 20})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
 
   def test_it_exists
@@ -24,5 +28,11 @@ class VehicleTest < Minitest::Test
 
   def test_it_starts_with_no_passengers
     assert_empty @vehicle.passengers
+  end
+
+  def test_it_can_add_passengers
+    @vehicle.add_passenger(@charlie)
+
+    assert_equal [@charlie], @vehicle.passengers
   end
 end
