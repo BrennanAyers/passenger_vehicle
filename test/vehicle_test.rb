1a4d04c75b5aa4b4e96ecdc7f0aa4fcce3c3bd5f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-14 15:21:46 -0600

    Add initial Vehicle class with base tests

---
+++ test/vehicle_test.rb
@@ -0,0 +1,17 @@
+require './test/test_helper'
+require './lib/vehicle'
+class VehicleTest < Minitest::Test
+  def setup
+    @vehicle = Vehicle.new("2001", "Honda", "Civic")
+  end
+
+  def test_it_exists
+    assert_instance_of Vehicle, @vehicle
+  end
+
+  def test_it_has_argument_attributes
+    assert_equal "2001", @vehicle.year
+    assert_equal "Honda", @vehicle.make
+    assert_equal "Civic", @vehicle.model
+  end
+end
