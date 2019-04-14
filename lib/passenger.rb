e0c45cf1fb5606ef09f00dec5b52143ff91b14e4
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-14 15:16:33 -0600

    Add driver attr, drive method, Fix adult? returns

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -4,10 +4,19 @@ class Passenger
   def initialize(info)
     @name = info["name"]
     @age = info["age"]
+    @driver = false
   end
 
   def adult?
-    true if age >= 18
+    return true if age >= 18 
     false if age < 18
+  end
+
+  def driver?
+    @driver
+  end
+
+  def drive
+    @driver = true
   end
 end
