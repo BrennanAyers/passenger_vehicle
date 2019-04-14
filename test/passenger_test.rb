a5a5ca711af5b1eb81cfceacca124e50ce31c4b4
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-14 13:23:14 -0600

    Add initial Passenger class tests

---
+++ test/passenger_test.rb
@@ -0,0 +1,18 @@
+require './test/test_helper'
+require './lib/passenger'
+class PassengerTest < Minitest::Test
+  def setup
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  def test_it_exists
+    assert_instance_of Passenger, @charlie
+    assert_instance_of Passenger, @taylor
+  end
+
+  def test_it_has_argument_attributes
+    assert_equal "Charlie", @charlie.name
+    assert_equal 18, @charlie.age
+  end
+end
